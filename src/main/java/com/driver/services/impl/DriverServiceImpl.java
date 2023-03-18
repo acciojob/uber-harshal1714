6738e9e21e6f253601639badce91126bee32e010
Harshal Kamdi <118383371+harshal1714@users.noreply.github.com>
2023-03-18 22:27:23 +0530

    my 1st change

--- src/main/java/com/driver/services/impl/DriverServiceImpl.java
+++ src/main/java/com/driver/services/impl/DriverServiceImpl.java
@@ -21,18 +21,31 @@ public class DriverServiceImpl implements DriverService {
 	@Override
 	public void register(String mobile, String password){
 		//Save a driver in the database having given details and a cab with ratePerKm as 10 and availability as True by default.
-
+      Driver driver=new Driver();
+      Cab cab=new Cab();
+	  //set can rate & availability using its object
+	  cab.setPerKmRate(10);
+	  cab.setAvailable(true);
+	  //update driver mobNo & pass using its object
+	  driver.setMobile(mobile);
+	  driver.setPassword(password);
+	  driver.setCab(cab);//set cab for a driver
+		driverRepository3.save(driver);
 	}
 
 	@Override
 	public void removeDriver(int driverId){
 		// Delete driver without using deleteById function
-
+		Driver driver=driverRepository3.findById(driverId).get();
+         driverRepository3.delete(driver);
 	}
 
 	@Override
 	public void updateStatus(int driverId){
 		//Set the status of respective car to unavailable
-
+      Driver driver=driverRepository3.findById(driverId).get();
+	  //for a particular cab set driver unavailable(false)
+	 driver.getCab().setAvailable(false);
+	 driverRepository3.save(driver);
 	}
 }
